136c1b9eb35882f0418eb3ba6d7f316dd5b7521d
MuratGuld <slimplus1952@gmail.com>
2022-10-23 13:01:27 +0200

    Todo service created and the data in todo component is got by this service

--- firstProject/src/app/components/todo/todo.component.ts
+++ firstProject/src/app/components/todo/todo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/todo';
 import { HttpClient } from '@angular/common/http';
 import { TodoResponseModel } from 'src/app/models/todoResponseModel';
+import { TodoService } from 'src/app/services/todo.service';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -9,17 +10,18 @@ import { TodoResponseModel } from 'src/app/models/todoResponseModel';
 })
 export class TodoComponent implements OnInit {
   todos: Todo[] = [];
+  dataLoaded: boolean = false;
 
-  apiUrl: string = 'https://jsonplaceholder.typicode.com/todos';
-  constructor(private httpClient: HttpClient) {}
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
     this.getTodos();
   }
 
   getTodos() {
-    this.httpClient.get<Todo[]>(this.apiUrl).subscribe((response) => {
+    this.todoService.getTodos().subscribe((response) => {
       this.todos = response;
+      this.dataLoaded = true;
     });
   }
 }
